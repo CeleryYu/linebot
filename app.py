59f707118230e26c84e1a3ba1c0c1cb05dea5224
CeleryYu <134586752+CeleryYu@users.noreply.github.com>
2023-12-09 22:22:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,10 +22,10 @@ handler = WebhookHandler('Y84c10630243d396fea192496c7268d16')
 line_bot_api.push_message('U29fccb6e017276d8f643b99962307fb5', TextSendMessage(text='你可以開始了'))
 # 要發送的訊息
 
-messages = [TextSendMessage(text='早安，又是新的一天，麻煩您回答下列問題喔。')]
+#messages = [TextSendMessage(text='早安，又是新的一天，麻煩您回答下列問題喔。')]
 
 # 發送廣播消息
-response = line_bot_api.broadcast(messages=messages)
+#response = line_bot_api.broadcast(messages=messages)
 
 
 #嘗試結束
@@ -37,7 +37,8 @@ def handle_message(event):
     if re.match('告訴我秘密',message):
         line_bot_api.reply_message(event.reply_token,TextSendMessage('才不告訴妳喔'))
     else:
-         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+
 
 # Webhook endpoint
 @app.route("/callback", methods=['POST'])
