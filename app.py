08ceaf8aed5a7a87f20b9f619f8c1bc79b8d9fdd
CeleryYu <134586752+CeleryYu@users.noreply.github.com>
2023-12-09 22:57:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -64,5 +64,6 @@ def callback():
 #主程式
 import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    #port = int(os.environ.get('PORT', 5000))
+     #app.run(host='0.0.0.0', port=port)
+     app.run()
