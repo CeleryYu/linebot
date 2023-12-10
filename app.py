f19a3040fc7e94632626baa24d1dddb4677a1a89
CeleryYu <134586752+CeleryYu@users.noreply.github.com>
2023-12-10 15:04:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,18 +15,22 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('ofZ8Zu8xLMIp7k/CqUN8amL45Sahn7t4w7ZhndSBk+e7HISzHG7V1sZe52IR65c3euwW/utIMmSaGxUMgJnp1U6MFq/fNh3ERB3+dlx31pxfZNqf9oi2s+1+UwlRu2Hy8HllGCAPbScdIr/ZAPpPCQdB04t89/1O/w1cDnyilFU=')
 # 必須放上自己的Channel Secret
 handler = WebhookHandler('Y84c10630243d396fea192496c7268d16')
-
-line_bot_api.push_message('U29fccb6e017276d8f643b99962307fb5', [
-    TextSendMessage(text='你可以開始了'),
-    TextSendMessage(text='這是推播的內容')
-])
-
-# 要發送的訊息
-messages = [TextSendMessage(text='壓嗨')]
+#廣播測試
+message = TextSendMessage(text='這是一則主動傳送的訊息')
 
 # 發送廣播消息
-response = line_bot_api.broadcast(messages=messages)
+response = line_bot_api.broadcast(messages=message)
 
+# 檢查是否成功
+if response.status_code == 200:
+    print("廣播消息發送成功！")
+else:
+    print(f"廣播消息發送失敗，錯誤碼：{response.status_code}")
+    print(response.json())
+
+#測試結束
+
+#回復測試
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     user_response = event.message.text
@@ -40,7 +44,7 @@ def handle_message(event):
 
     # 回覆使用者
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
-
+#測試結束
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
