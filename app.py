7ac9af2552a6e178f317b1a299b03bf27e3e9084
CeleryYu <134586752+CeleryYu@users.noreply.github.com>
2023-12-11 11:05:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -41,17 +41,9 @@ line_bot_api.push_message('U5f5c99cca72d8bb1d3111c3a00e03cea', TextSendMessage(t
 # 發送廣播消息
 #response = line_bot_api.broadcast(messages=messages)
 
-#try2
-# 定義關鍵字和回覆內容
-from linebot.models import TextSendMessage
-
 questions = {
-    '1': {'question': '請問您昨晚的睡眠如何？ 1.很好 2.一般 3.不好'},
-    '2': {'question': '您的心情跟平時比起來如何呢？1.很好 2.一般 3.不好'},
-    '3': {'question': '您的身體狀況跟平時比起來如何呢？1.好 2.不好'},
-    '4': {'question': '您是哪個部位不舒服呢？1.頭 2.脖子 3.手 4.腳 5.背 6.腰 7.心臟'},
-    '5': {'question': '其部位跟平時比起來的疼痛度為何？1.一直都這樣 2.突然開始痛'},
-    '6': {'question': '今天的食慾跟平時比起來如何？1.很好 2.一般 3.不好'}
+    '1': {'question': '您是哪個部位不舒服呢？1.頭 2.脖子 3.手 4.腳 5.背 6.腰 7.心臟'},
+    '2': {'question': '其部位跟平時比起來的疼痛度為何？1.一直都這樣 2.突然開始痛'}
 }
 
 current_question_number = 1  # 初始問題編號
@@ -64,36 +56,32 @@ def handle_user_response(user_response):
     current_question = questions.get(str(current_question_number))
 
     if current_question:
-        # 更新問題編號
-        current_question_number += 1
-
-        # 根據使用者回答處理下一個問題
-        next_question = handle_next_question(current_question_number, user_response)
-
-        return TextSendMessage(text=next_question)
+        if current_question_number == 1 and user_response == '1':
+            return TextSendMessage(text='謝謝您的回答')
+        elif current_question_number == 1 and user_response == '2':
+            # 回傳第一個問題的回答，然後更新問題編號
+            current_question_number += 1
+            return TextSendMessage(text=current_question['question'])
+        elif current_question_number == 2:
+            # 回傳第二個問題的回答，不論使用者回答什麼，都繼續回傳第二個問題
+            current_question_number += 1
+            return TextSendMessage(text=current_question['question'])
+        else:
+            # 其他情況
+            current_question_number += 1
+            return TextSendMessage(text='謝謝您的回答')
     else:
         return TextSendMessage(text='謝謝您的回答！')
 
-# 根據問題編號處理下一個問題的函數
-def handle_next_question(question_number, user_response):
-    # 在這裡實現你的邏輯，根據問題編號和使用者回答來返回下一個問題的內容
-    # 這只是一個簡單的例子，你可以根據實際需求進行修改
-    if question_number == 2:
-        if user_response == '1':
-            return '好的，心情很好！下一個問題...'
-        elif user_response == '2':
-            return '一般般，繼續下一個問題...'
-        elif user_response == '3':
-            return '不太好，請告訴我原因...'
-    elif question_number == 3:
-        # 添加第三個問題的邏輯...
-
 # 在使用者回答時呼叫處理函數
-user_response = '1'  # 使用者回答的內容
+user_response = '2'  # 使用者回答的內容
 response_message = handle_user_response(user_response)
 
 # 將回應訊息發送給使用者
-print(response_message.text)
+#print(response_message.text)
+
+
+
 
 
     # 如果訊息中沒有任何關鍵字，可以回覆預設訊息或不回覆
