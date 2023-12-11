ee824697dc69ee7934fe41987d2e604fd9373942
CeleryYu <134586752+CeleryYu@users.noreply.github.com>
2023-12-11 10:32:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -40,45 +40,7 @@ messages = [TextSendMessage(text='我是朱虹聿，這linebot被我劫持了。
 
 # 發送廣播消息
 response = line_bot_api.broadcast(messages=messages)
-#try2
 
-image_urls = [
-    'https://drive.google.com/file/d/1RHygPRwNg0bdiN51i35X1B7XefC6FNW2/view?usp=sharing',
-    'https://drive.google.com/file/d/1lqrH_34kf-J50LMXO9aidJMYmOYHYGkB/view?usp=sharing',
-    'https://drive.google.com/file/d/1iM9CqTPbdwA3oIi8ZmcYzvetxLfdr2CP/view?usp=sharing',
-    'https://drive.google.com/file/d/1ChEFRM_EBDD9fV7UNAlNEJSaTJS714qw/view?usp=sharing',
-    'https://drive.google.com/file/d/1V9g79W8tuMH_TP4Ce54dgmdSs91I0LXp/view?usp=sharing',
-    'https://drive.google.com/file/d/1U7VInWvCxUtlCL9Xs6MqLnLbC0OOL2dh/view?usp=sharing',
-    'https://drive.google.com/file/d/1LffGzf3uyg66igeiqyCRVwic8EPz5r6h/view?usp=sharing',
-    'https://drive.google.com/file/d/1zWz0lplkaHbwIV583XIQ5TshiYzI1bek/view?usp=sharing',
-    'https://drive.google.com/file/d/1U4wXJRYFmEYtYJSJfSocQ9ATylJAt8NF/view?usp=sharing',
-    'https://drive.google.com/file/d/1htUL52vKgiCyz8LbirhhqIck8jYTkzA_/view?usp=sharing',
-    'https://drive.google.com/file/d/1wBmzZW9Vk562fCnZNMo13udNWAkQhgnL/view?usp=sharing',
-    'https://drive.google.com/file/d/1x_-aqNYji_Z2JkH9YBeGL9C13b6ahIr8/view?usp=sharing',
-    'https://drive.google.com/file/d/16LkzQ_yXmJInigzVkBpsH346ikF_j1q4/view?usp=sharing',
-    'https://drive.google.com/file/d/1EKG6fMV15vCCXJHX-I6sCxLP7DFOs09Q/view?usp=sharing',
-    'https://drive.google.com/file/d/1GWczzcsioSeKtNbbIc7Qv1v02Cbr81BG/view?usp=sharing',
-    'https://drive.google.com/file/d/1uCpyX5Btnh3IF9BGs2ULgAFJY850X6HP/view?usp=sharing',
-    'https://drive.google.com/file/d/1S8_jFsJc9Ob_-XclL48n6k3uMgma74R1/view?usp=sharing',
-    'https://drive.google.com/file/d/1M78gGbo0aJ9djfX89kHarhy7v7TAuam2/view?usp=sharing',
-    'https://drive.google.com/file/d/1F7SlRsvpzxU58VoWIyO8LWRj5DKyjSPg/view?usp=sharing',
-    'https://drive.google.com/file/d/1bT9i-inItuGN12StnBaaPRKzectuRPsP/view?usp=sharing'
-    # 添加更多連結...
-]
-
-def send_scheduled_message():
-    # 隨機選擇一個圖片連結
-
-
-    selected_image_url = random.choice(image_urls)
-
-    # 發送圖片給用戶
-    line_bot_api.broadcast(
-        ImageSendMessage(original_content_url=selected_image_url, preview_image_url=selected_image_url)
-    )
-
-# 設定每天時間執行一次
-schedule.every().day.at("20:45").do(send_scheduled_message)
 #try2
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
